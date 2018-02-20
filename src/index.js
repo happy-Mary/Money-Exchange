cbb58bcbdd1157573adf89c16ee135ae43be4c46
Maria <mariashklovskaya@coherentsolutions.com>
2018-02-20 18:48:58 +0300

    rolling-scope/feature: exchange function ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const allowedCurr = { H: 50, Q: 25, D: 10, N: 5, P: 1 };
+    let obj = {};
+
+    if(currency <= 0) {
+        obj = {};
+    } else if (currency >= 10000) {
+        obj = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+        for(let key in allowedCurr) {
+            let value = allowedCurr[key];
+            let x = parseInt(currency/value);
+            if(x > 0) { obj[key] = x; }
+            currency = currency%value;
+        }
+    }
+    return obj;
 }
+
